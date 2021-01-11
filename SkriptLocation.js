b85a06df7fd5ce3bd54f2d35d3f1b440fb83ad30
XMaceska <dave.maceska@seznam.cz>
2021-01-12 00:53:17 +0100

    test nevim ceho zatim

---
+++ SkriptLocation.js
@@ -0,0 +1,36 @@
+window.onload = () => {
+     let places = staticLoadPlaces();
+     renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+    return [
+        {
+            name: 'church',
+            location: {
+                lat: 50.0930400,
+                lng: 14.3818325,
+            }
+        },
+    ];
+}
+function renderPlaces(places) {
+    let scene = document.querySelector('a-scene');
+
+    places.forEach((place) => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
+
+        let model = document.createElement('a-entity');
+        model.setAttribute('gps-projected-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        model.setAttribute('gltf-model','./church/church.gltf');
+        model.setAttribute('animation-mixer', '');
+        model.setAttribute('scale', '1 1 1');
+
+        model.addEventListener('loaded', () => {
+            window.dispatchEvent(new CustomEvent('gps-projected-entity-place-loaded'))
+        });
+
+        scene.appendChild(model);
+    });
+}
